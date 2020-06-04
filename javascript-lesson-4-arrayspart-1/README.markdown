6a556ab5355a2055b4aa15bb56e2c2266ef4beb8
Fonz2424 <65732402+Fonz2424@users.noreply.github.com>
2020-06-04 15:46:05 -0400

    JavaScript-Lesson-4-Arrays- Part-1

---
+++ javascript-lesson-4-arrayspart-1/README.markdown
@@ -0,0 +1,5 @@
+# JavaScript-Lesson-4-Arrays- Part-1
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/Fonz2424/pen/pogvPgq](https://codepen.io/Fonz2424/pen/pogvPgq).
+
+
---
+++ javascript-lesson-4-arrayspart-1/dist/script.js
@@ -0,0 +1,9 @@
+//array with 5 strings
+const songList = ["Paper Right","The Requiem Of Nat Turner","The Mula","Mastered","Casino"];
+
+//learning index placement in arrays 0 is 1 and so for
+console.log(songList[0]);
+console.log(songList[1]);
+console.log(songList[2]);
+console.log(songList[3]);
+console.log(songList[4]);
